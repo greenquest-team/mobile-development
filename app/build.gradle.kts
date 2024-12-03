875a3d9174b1247f6e03251064e2d24b26f727a0
wahyuagastya <wahyuagastyakomang@gmail.com>
2024-12-03 18:41:18 +0800

    push layout, gambar sebagian, dan dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,11 +75,12 @@ dependencies {
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
 
-
     //to make rounded image view
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.eightbitlab:blurview:1.6.6") //blur view
     implementation("com.mikhaellopez:circularprogressbar:3.1.0") //progress bar-circle
+
+    implementation("com.google.android.material:material:1.9.0")
 
     implementation(libs.tensorflow.lite)
     implementation(libs.tensorflow.lite.api)
